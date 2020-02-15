31e48955f6a956c7166d0c45a27e0a7a37523d44
Alan Swircenski <ahswircenski@gmail.com>
2020-02-15 10:55:46 -0900

    34. Static Class (Singleton Design Pattern)

--- My Magic Destroyers/My Magic Destroyers/Program.cs
+++ My Magic Destroyers/My Magic Destroyers/Program.cs
@@ -18,6 +18,16 @@ namespace My_Magic_Destroyers
             Console.WriteLine(secondWarlock.Name + ", there are " + Warlock.IdCounter + " warlocks and I have ID# " + secondWarlock.Id);
             Console.WriteLine(thirdWarlock.Name + ", there are " + Warlock.IdCounter + " warlocks and I have ID# " + thirdWarlock.Id);
             Console.WriteLine(firstWarlock.Name + ", there are " + Warlock.IdCounter + " warlocks and I have ID# " + firstWarlock.Id);
+            Console.WriteLine(  );
+
+            var text1 = firstWarlock.Name + ", there are " + Warlock.IdCounter + " warlocks and I have ID# " + firstWarlock.Id;
+            var text2 = secondWarlock.Name + ", there are " + Warlock.IdCounter + " warlocks and I have ID# " + secondWarlock.Id;
+            var text3 = thirdWarlock.Name + ", there are " + Warlock.IdCounter + " warlocks and I have ID# " + thirdWarlock.Id;
+            Tools.ColorfulWriteLine(text1, ConsoleColor.Red);
+            Tools.ColorfulWriteLine(text2, ConsoleColor.Green);
+            Tools.ColorfulWriteLine(text3, ConsoleColor.Yellow);
+
+
 
             Console.WriteLine(Environment.NewLine + firstWarlock.Name + "'s bodyarmor has " + firstWarlock.BodyArmor.ArmorPoints + " points. His sword can do " + firstWarlock.Weapon.Damage + " points of damage.");
 
